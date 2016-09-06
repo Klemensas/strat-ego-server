3c0fde44b2812061631e5cfd6556bc0717445318
Klemensas <kdranseika@gmail.com>
2016-09-06 23:50:47 +0300

    Add user name to auth token

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -101,20 +101,20 @@ export function hasRole(roleRequired) {
 /**
  * Returns a jwt token signed by the app secret
  */
-export function signToken(id, role) {
-  return jwt.sign({ _id: id, role: role }, config.secrets.session, {
-    expiresIn: 60 * 60 * 5
+export function signToken(name, _id, role) {
+  return jwt.sign({ name, _id, role }, config.secrets.session, {
+    expiresIn: 60 * 60 * 5,
   });
 }
 
 /**
  * Set token cookie directly for oAuth strategies
  */
-export function setTokenCookie(req, res) {
+export function setTokenCookie  (req, res) {
   if (!req.user) {
     return res.status(404).send('It looks like you aren\'t logged in, please try again.');
   }
-  var token = signToken(req.user._id, req.user.role);
+  const token = signToken(req.user.name, req.user._id, req.user.role);
   res.cookie('token', token);
   res.redirect('/');
 }
