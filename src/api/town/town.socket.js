13d1b650d64ae049eb31c287d9df0d782403ab8d
Klemensas <kdranseika@gmail.com>
2016-09-18 23:36:02 +0300

    Start building implementation

--- src/api/town/town.socket.js
+++ src/api/town/town.socket.js
@@ -1,6 +1,17 @@
+function getTown(client, data) {
+  const targetTown = client.player.Towns.find(t => data.town === t._id);
+  return targetTown;
+}
+
+function tryBuilding(town, data) {
+  // check if building exists
+  if (town.buildings[data.building]) {
+    console.log('ok building exists');
+  }
+}
 
 function changeName(data) {
-  let targetTown = this.player.Towns.find(t => data.town === t._id);
+  let targetTown = getTown(this, data);
   if (!targetTown || !data.name) {
     // handle missing town / no name error
     // this.disconnect();
@@ -10,13 +21,28 @@ function changeName(data) {
   targetTown.fullSave()
     .then(town => {
       targetTown = town;
-      this.log('town saved')
+      this.log('town saved');
       this.emit('town', town);
     })
     .catch(err => {
-      console.log('---save fail', err)
+      console.log('---save fail', err);
     });
 }
+
+function build(data) {
+  let targetTown = getTown(this, data);
+  if (!targetTown) {
+    // handle missing town / no name error
+    // this.disconnect();
+    return;
+  }
+  tryBuilding(targetTown, data);
+  // targetTown.buildings[data.building]
+  // targetTown.fullSave()
+  // targetTown.buildings[]
+}
+
+
 
 export const register = socket => {
 
@@ -27,5 +53,6 @@ export const register = socket => {
   //   worldEvents.player.on(event, listener);
   //   socket.on('disconnect', removeListener(event, listener));
   socket.on('town:name', changeName);
+  socket.on('town:build', build);
   // }
 };
