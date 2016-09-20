0513bc3757afd805dd2cd3140b87234794492dc6
Klemensas <kdranseika@gmail.com>
2016-09-20 23:51:09 +0300

    Town socket add building queuing logic

--- src/api/town/town.socket.js
+++ src/api/town/town.socket.js
@@ -1,3 +1,8 @@
+import { activeWorlds } from '../../components/worlds';
+import { world } from '../../sqldb';
+
+const BuildingQueue = world.BuildingQueue;
+
 function getTown(client, data) {
   const targetTown = client.player.Towns.find(t => data.town === t._id);
   return targetTown;
@@ -5,9 +10,40 @@ function getTown(client, data) {
 
 function tryBuilding(town, data) {
   // check if building exists
-  if (town.buildings[data.building]) {
-    console.log('ok building exists');
+  const target = town.buildings[data.building];
+  if (target) {
+    // Select next level latest queued or the current level;
+    const level = target.queued || target.level;
+    // const world = activeWorlds.get('megapolis');
+    // get data for target building
+    const townWorld = activeWorlds.get('megapolis');
+    const buildingData = townWorld.buildingData
+      .find(building => building.name === data.building).data[level];
+
+    // check if building has target level
+    // TODO: add and check requirements somewhere here
+    if (buildingData) {
+      town.resources.clay -= buildingData.costs.clay;
+      town.resources.wood -= buildingData.costs.wood;
+      town.resources.iron -= buildingData.costs.iron;
+      target.queued = level + 1;
+      // trigger buildings change manully, because sequalize can't detect it
+      town.changed('buildings', true);
+      // save town with updated values
+      return town.save()
+        // create buildingQueue
+        .then(updatedTown => updatedTown.createBuildingQueue({
+          building: data.building,
+          buildTime: buildingData.buildTime,
+          endsAt: updatedTown.updatedAt + buildingData.buildTime * 1000,
+          level,
+        }))
+        // reload town to fetch association and new values
+        .then(() => town.reload({ include: [{ model: BuildingQueue }] }));
+    }
   }
+  // TODO: real error here
+  return Promise.reject('target not found');
 }
 
 function changeName(data) {
@@ -18,7 +54,7 @@ function changeName(data) {
     return;
   }
   targetTown.name = data.name;
-  targetTown.fullSave()
+  targetTown.save()
     .then(town => {
       targetTown = town;
       this.log('town saved');
@@ -36,12 +72,15 @@ function build(data) {
     // this.disconnect();
     return;
   }
-  tryBuilding(targetTown, data);
-  // targetTown.buildings[data.building]
-  // targetTown.fullSave()
-  // targetTown.buildings[]
+  tryBuilding(targetTown, data)
+    .then(town => {
+      targetTown = town;
+      this.emit('town', town);
+    })
+    .catch(error => {
+      console.log('SOCKET ERROR ERROR', error);
+    });
 }
-
 
 
 export const register = socket => {
