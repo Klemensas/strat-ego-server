69bd800449ddee126a3369a7cb79a7efce612814
Klemensas <kdranseika@gmail.com>
2016-09-28 23:52:29 +0300

    Format buildingData; subtract base to account for power of 0

--- src/config/game/buildingData.js
+++ src/config/game/buildingData.js
@@ -1,200 +1,203 @@
-const buildingList = [{
-  name: 'headquarters',
-  levels: 30,
-  min: 1,
-  costs: {
-    wood: {
-      base: 90,
-      factor: 1.26,
+const buildingList = [
+  {
+    name: 'headquarters',
+    levels: 30,
+    min: 1,
+    costs: {
+      wood: {
+        base: 90,
+        factor: 1.26,
+      },
+      clay: {
+        base: 80,
+        factor: 1.275,
+      },
+      iron: {
+        base: 70,
+        factor: 1.26,
+      },
     },
-    clay: {
-      base: 80,
-      factor: 1.275,
+    baseTime: 20,
+    timeFactor: 1.2,
+  }, {
+    name: 'barracks',
+    levels: 25,
+    min: 0,
+    costs: {
+      wood: {
+        base: 200,
+        factor: 1.26,
+      },
+      clay: {
+        base: 170,
+        factor: 1.28,
+      },
+      iron: {
+        base: 90,
+        factor: 1.26,
+      },
     },
-    iron: {
-      base: 70,
-      factor: 1.26,
+    baseTime: 30,
+    timeFactor: 1.2,
+  }, {
+    name: 'wood',
+    levels: 30,
+    min: 0,
+    costs: {
+      wood: {
+        base: 50,
+        factor: 1.25,
+      },
+      clay: {
+        base: 60,
+        factor: 1.275,
+      },
+      iron: {
+        base: 40,
+        factor: 1.245,
+      },
+    },
+    baseTime: 15,
+    timeFactor: 1.2,
+    additional: {
+      production: {
+        base: 30,
+        factor: 1.163118,
+      },
+    },
+  }, {
+    name: 'clay',
+    levels: 30,
+    min: 0,
+    costs: {
+      wood: {
+        base: 65,
+        factor: 1.27,
+      },
+      clay: {
+        base: 50,
+        factor: 1.265,
+      },
+      iron: {
+        base: 40,
+        factor: 1.24,
+      },
+    },
+    baseTime: 15,
+    timeFactor: 1.2,
+    additional: {
+      production: {
+        base: 30,
+        factor: 1.163118,
+      },
+    },
+  }, {
+    name: 'iron',
+    levels: 30,
+    min: 0,
+    costs: {
+      wood: {
+        base: 75,
+        factor: 1.252,
+      },
+      clay: {
+        base: 65,
+        factor: 1.275,
+      },
+      iron: {
+        base: 70,
+        factor: 1.24,
+      },
+    },
+    baseTime: 18,
+    timeFactor: 1.2,
+    additional: {
+      production: {
+        base: 30,
+        factor: 1.163118,
+      },
+    },
+  }, {
+    name: 'wall',
+    levels: 20,
+    min: 0,
+    costs: {
+      wood: {
+        base: 50,
+        factor: 1.26,
+      },
+      clay: {
+        base: 100,
+        factor: 1.275,
+      },
+      iron: {
+        base: 20,
+        factor: 1.24,
+      },
+    },
+    baseTime: 60,
+    timeFactor: 1.2,
+    additional: {
+      defense: {
+        base: 0,
+        factor: 0,
+      },
+    },
+  }, {
+    name: 'storage',
+    levels: 30,
+    min: 1,
+    costs: {
+      wood: {
+        base: 60,
+        factor: 1.265,
+      },
+      clay: {
+        base: 50,
+        factor: 1.27,
+      },
+      iron: {
+        base: 40,
+        factor: 1.245,
+      },
+    },
+    baseTime: 17,
+    timeFactor: 1.2,
+    additional: {
+      storage: {
+        base: 1000,
+        factor: 1.2294934,
+      },
+    },
+  }, {
+    name: 'farm',
+    levels: 30,
+    min: 1,
+    costs: {
+      wood: {
+        base: 45,
+        factor: 1.3,
+      },
+      clay: {
+        base: 40,
+        factor: 1.32,
+      },
+      iron: {
+        base: 30,
+        factor: 1.29,
+      },
+    },
+    baseTime: 20,
+    timeFactor: 1.2,
+    additional: {
+      population: {
+        base: 240,
+        factor: 1.172103,
+      },
     },
   },
-  baseTime: 20,
-  timeFactor: 1.2,
-}, {
-  name: 'barracks',
-  levels: 25,
-  min: 0,
-  costs: {
-    wood: {
-      base: 200,
-      factor: 1.26,
-    },
-    clay: {
-      base: 170,
-      factor: 1.28,
-    },
-    iron: {
-      base: 90,
-      factor: 1.26,
-    },
-  },
-  baseTime: 30,
-  timeFactor: 1.2,
-}, {
-  name: 'wood',
-  levels: 30,
-  min: 0,
-  costs: {
-    wood: {
-      base: 50,
-      factor: 1.25,
-    },
-    clay: {
-      base: 60,
-      factor: 1.275,
-    },
-    iron: {
-      base: 40,
-      factor: 1.245,
-    },
-  },
-  baseTime: 15,
-  timeFactor: 1.2,
-  additional: {
-    production: {
-      base: 30,
-      factor: 1.163118,
-    },
-  },
-}, {
-  name: 'clay',
-  levels: 30,
-  min: 0,
-  costs: {
-    wood: {
-      base: 65,
-      factor: 1.27,
-    },
-    clay: {
-      base: 50,
-      factor: 1.265,
-    },
-    iron: {
-      base: 40,
-      factor: 1.24,
-    },
-  },
-  baseTime: 15,
-  timeFactor: 1.2,
-  additional: {
-    production: {
-      base: 30,
-      factor: 1.163118,
-    },
-  },
-}, {
-  name: 'iron',
-  levels: 30,
-  min: 0,
-  costs: {
-    wood: {
-      base: 75,
-      factor: 1.252,
-    },
-    clay: {
-      base: 65,
-      factor: 1.275,
-    },
-    iron: {
-      base: 70,
-      factor: 1.24,
-    },
-  },
-  baseTime: 18,
-  timeFactor: 1.2,
-  additional: {
-    production: {
-      base: 30,
-      factor: 1.163118,
-    },
-  },
-}, {
-  name: 'wall',
-  levels: 20,
-  min: 0,
-  costs: {
-    wood: {
-      base: 50,
-      factor: 1.26,
-    },
-    clay: {
-      base: 100,
-      factor: 1.275,
-    },
-    iron: {
-      base: 20,
-      factor: 1.24,
-    },
-  },
-  baseTime: 60,
-  timeFactor: 1.2,
-  additional: {
-    defense: {
-      base: 0,
-      factor: 0,
-    },
-  },
-}, {
-  name: 'storage',
-  levels: 30,
-  min: 1,
-  costs: {
-    wood: {
-      base: 60,
-      factor: 1.265,
-    },
-    clay: {
-      base: 50,
-      factor: 1.27,
-    },
-    iron: {
-      base: 40,
-      factor: 1.245,
-    },
-  },
-  baseTime: 17,
-  timeFactor: 1.2,
-  additional: {
-    storage: {
-      base: 1000,
-      factor: 1.2294934,
-    },
-  },
-}, {
-  name: 'farm',
-  levels: 30,
-  min: 1,
-  costs: {
-    wood: {
-      base: 45,
-      factor: 1.3,
-    },
-    clay: {
-      base: 40,
-      factor: 1.32,
-    },
-    iron: {
-      base: 30,
-      factor: 1.29,
-    },
-  },
-  baseTime: 20,
-  timeFactor: 1.2,
-  additional: {
-    population: {
-      base: 240,
-      factor: 1.172103,
-    },
-  },
-}];
+];
+
 export const buildingData = (buildings = buildingList) => buildings.map(building => {
   const item = {
     name: building.name,
@@ -214,7 +217,7 @@ export const buildingData = (buildings = buildingList) => buildings.map(building
     if (building.additional) {
       for (const key in building.additional) {
         if (building.additional.hasOwnProperty(key)) {
-          data[key] = Math.ceil(building.additional[key].base * Math.pow(building.additional[key].factor, i));
+          data[key] = Math.ceil(building.additional[key].base * Math.pow(building.additional[key].factor, i) - building.additional[key].base);
         }
       }
     }
