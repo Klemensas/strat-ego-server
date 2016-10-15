916db5b14e1fe208c4c741894c38a8d1c61373c7
Klemensas <kdranseika@gmail.com>
2016-10-15 23:36:37 +0300

    Refactor queue to support both building and unit queues

--- src/api/world/queue.js
+++ src/api/world/queue.js
@@ -3,6 +3,7 @@ import { socket } from '../../app';
 
 const Town = world.Town;
 const BuildingQueue = world.BuildingQueue;
+const UnitQueue = world.UnitQueue;
 
 class Queue {
   constructor() {
@@ -10,23 +11,19 @@ class Queue {
 
     BuildingQueue
       .destroy({ where: { TownId: null } })
+      .then(() => UnitQueue.destroy({ where: { TownId: null } }))
       .then(() => BuildingQueue.findAll())
-      .then(items => items.forEach(this.queueItem));
+      .then(items => items.forEach(item => this.queueItem(item, 'building')))
+      .then(() => UnitQueue.findAll())
+      .then(items => items.forEach(item => this.queueItem(item, 'unit')));
   }
 
   processItem(townId) {
     const queueItem = this.items[townId].nextItem;
+    const queueAction = queueItem.type === 'building' ? this.processBuilding : this.processUnit;
     return Town.findOne({ where: { _id: townId } }, { include: { all: true } })
+      .then(town => queueAction(town, queueItem))
       .then(town => {
-        const building = town.buildings[queueItem.building];
-        building.level++;
-        // Set queued to 0 if queue is empty for building
-        if (building.queued === building.level) {
-          building.queued = 0;
-        }
-        // trigger buildings change manully, because sequalize can't detect it
-        town.changed('buildings', true);
-
         return world.sequelize.transaction(transaction => {
           return queueItem.destroy({ transaction })
             .then(() => town.save({ transaction }));
@@ -35,6 +32,27 @@ class Queue {
       })
       // TODO: test and handle concurrency
       .catch(error => console.log(`PROCESS ERROR: ${error}`));
+  }
+
+  processBuilding(town, item) {
+    const building = town.buildings[item.building];
+    building.level++;
+    // Set queued to 0 if queue is empty for building
+    if (building.queued === building.level) {
+      building.queued = 0;
+    }
+    // trigger buildings change manully, because sequalize can't detect it
+    town.changed('buildings', true);
+    return town;
+  }
+
+  processUnit(town, item) {
+    const unit = town.units[item.unit];
+    unit.amount += item.amount;
+    unit.queued -= item.amount;
+    // trigger units change manully, because sequalize can't detect it
+    town.changed('units', true);
+    return town;
   }
 
   updateQueue(id, item) {
@@ -51,10 +69,11 @@ class Queue {
     townQueue.items.splice(position, 1);
   }
 
-  queueItem(item) {
+  queueItem(item, type) {
     const town = item.TownId;
     let townQueue = this.items[town];
     item.endsAt = new Date(item.endsAt).getTime();
+    item.type = type;
 
     if (townQueue && townQueue.nextItem) {
       townQueue.items.push(item);
