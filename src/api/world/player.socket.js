afebbc702c15ccfe9e960c0488c16e150efb04e3
Klemensas <kdranseika@gmail.com>
2017-03-30 00:28:03 +0300

    Refactor player creation; include only relevant data on incoming movements

--- src/api/world/player.socket.js
+++ src/api/world/player.socket.js
@@ -4,44 +4,55 @@ import * as map from '../../components/map';
 const UserWorlds = main.UserWorlds;
 const Player = world.Player;
 const Town = world.Town;
-const BuildingQueue = world.BuildingQueue;
+const Movement = world.Movement;
 
 function createPlayer(socket) {
-  return player => {
-    if (player) {
-      return player;
-    }
-    socket.log(`creating player for ${socket.username}, on ${socket.world}`);
-    return map.chooseLocation(socket.world)
-      .then(location => Player.create({
-        name: socket.username,
+  socket.log(`creating player for ${socket.username}, on ${socket.world}`);
+  return map.chooseLocation(socket.world)
+    .then(location => Player.create({
+      name: socket.username,
+      UserId: socket.userId,
+      Towns: [{
+        name: `${socket.username}s Town`,
+        location,
+      }],
+    }, {
+      include: [{ all: true }],
+    }))
+    .then(newPlayer => {
+      return UserWorlds.create({
         UserId: socket.userId,
-        Towns: [{
-          name: `${socket.username}s Town`,
-          location,
-        }],
-      }, {
-        include: [{ all: true }],
-      }))
-      .then(newPlayer => {
-        return UserWorlds.create({
-          UserId: socket.userId,
-          World: socket.world,
-          PlayerId: newPlayer._id,
-        })
-        .then(() => newPlayer);
-      });
-  };
+        World: socket.world,
+        PlayerId: newPlayer._id,
+      })
+      .then(() => newPlayer);
+    });
 }
 
 export default socket => Player.findOne({
   where: { UserId: socket.userId },
   include: [{
     model: Town,
-    include: [{ all: true }]
+    include: [{
+      all: true
+    }, {
+      model: Movement,
+      as: 'MovementDestinationTown',
+      attributes: { exclude: ['createdAt', 'updatedAt', 'units'] }
+    }]
   }]
 })
-  .then(createPlayer(socket))
+  .then(player => {
+    if (!player) {
+      return createPlayer(socket);
+    }
+    // TODO: process queues here before sending?
+    // player.Towns.map(town => {
+    //   console.log('t', town.dataValues, typeof town.processQueues);
+    // });
+    return player;
+  })
+  // .then(createPlayer(socket))
   .then(player => {
     socket.player = player;
     socket.emit('player', player);
