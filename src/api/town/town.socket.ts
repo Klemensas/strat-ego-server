453a4ed023c01f3df5d56d2bbccc05d167366e75
Klemensas <hello@klemensas.net>
2018-04-04 16:53:40 +0300

    Rework town socket

--- src/api/town/town.socket.ts
+++ src/api/town/town.socket.ts
@@ -1,11 +1,14 @@
-import * as Bluebird from 'bluebird';
-import { Town, townIncludes, Coords } from './town.model';
-import { Player } from '../world/player.model';
-import { UnitQueue } from '../world/unitQueue.model';
-import { world } from '../../sqldb';
-import { UserSocket, AuthenticatedSocket } from 'config/socket';
-import { MovementType } from 'api/town/movement.model';
+import { Coords, TownError, MovementType, MovementUnit } from 'strat-ego-common';
+import { transaction } from 'objection';
+
+import { io } from '../../';
+import { knexDb } from '../../sqldb';
+import { UserSocket, AuthenticatedSocket, ErrorMessage } from '../../config/socket';
 import { worldData } from '../world/worldData';
+import { Town } from './town';
+import { BuildingQueue } from '../building/buildingQueue';
+import { UnitQueue } from '../unit/unitQueue';
+import { Movement } from './movement';
 
 export interface SocketPayload { town: number; }
 export interface NamePayload extends SocketPayload { name: string; }
@@ -14,9 +17,8 @@ export interface PayloadUnit { type: string; amount: number; }
 export interface RecruitPayload extends SocketPayload {
   units: PayloadUnit[];
 }
-export type PayloadMovementUnit = [string, number];
 export interface TroopMovementPayload extends SocketPayload {
-  units: PayloadMovementUnit[];
+  units: MovementUnit[];
   type: MovementType;
   target: Coords;
 }
@@ -25,7 +27,7 @@ export class TownSocket {
   static onConnect(socket: UserSocket) {
     this.joinTownRoom(socket);
 
-    socket.on('town:name', (payload: NamePayload) => this.changeName(socket, payload));
+    socket.on('town:rename', (payload: NamePayload) => this.rename(socket, payload));
     socket.on('town:build', (payload: BuildPayload) => this.build(socket, payload));
     socket.on('town:recruit', (payload: RecruitPayload) => this.recruit(socket, payload));
     socket.on('town:moveTroops', (payload: TroopMovementPayload) => this.moveTroops(socket, payload));
@@ -36,183 +38,241 @@ export class TownSocket {
     socket.userData.townIds.forEach((id) => socket.join(String(id)));
   }
 
-  private static changeName(socket: UserSocket, payload: NamePayload) {
-    if (!payload.name || !payload.town) { return Promise.reject('Missing data'); }
-    if (!socket.userData.townIds.includes(payload.town)) { return Promise.reject('No town found'); }
-
-    return Town.getTown({ id: payload.town })
-      .then((town): PromiseLike<Town> => {
-        town.name = payload.name;
-        return town.save();
-      })
-      .then((town) => town.notify({ type: 'name' }));
+  static emitToTownRoom(room: number, payload: any, topic: string = 'town') {
+    const roomName = String(room);
+    io.sockets.in(roomName).emit(topic, payload);
   }
 
-  private static build(socket: UserSocket, payload: BuildPayload) {
-    if (!payload.building || !payload.town) { return Promise.reject('Missing data'); }
-    if (!socket.userData.townIds.includes(payload.town)) { return Promise.reject('No town found'); }
+  private static async rename(socket: UserSocket, payload: NamePayload) {
+    try {
+      if (!payload.name || !payload.town) { throw new ErrorMessage('Missing required data'); }
+      if (!socket.userData.townIds.includes(payload.town)) { throw new ErrorMessage('No town found'); }
 
-    const time = Date.now();
-    return Town.getTown({ id: payload.town })
-      .then((town) => this.tryBuilding(town, time, payload.building))
-      .then((town) => town.notify({ type: 'build' }));
+      const town = await Town.query(knexDb.world)
+        .patch({ name: payload.name })
+        .where({
+          id: payload.town,
+          playerId: socket.userData.playerId,
+        })
+        .eager(Town.townRelations);
+      this.emitToTownRoom(payload.town, payload.name, 'town:renameSuccess');
+    } catch (err) {
+      socket.handleError(err, 'name', 'town:renameFail', payload);
+    }
   }
 
-  private static recruit(socket: UserSocket, payload: RecruitPayload) {
-    if (!payload.units || !payload.town) { return Promise.reject('Missing data'); }
-    if (!socket.userData.townIds.includes(payload.town)) { return Promise.reject('No town found'); }
+  private static async build(socket: UserSocket, payload: BuildPayload) {
+    try {
+      if (!payload.building || !payload.town) { throw new ErrorMessage('Missing required data'); }
+      if (!socket.userData.townIds.includes(payload.town)) { throw new ErrorMessage('No town found'); }
 
-    const time = Date.now();
-    return Town.getTown({ id: payload.town})
-      .then((town) => this.tryRecruiting(town, time, payload.units))
-      .then((town) => town.notify({ type: 'recruit' }));
-  }
-  private static moveTroops(socket: UserSocket, payload: TroopMovementPayload) {
-    if (!payload.town || !payload.target || !payload.type) { return; }
-    if (!socket.userData.townIds.includes(payload.town)) { return Promise.reject('No town found'); }
-
-    const time = Date.now();
-    return Town.getTown({ id: payload.town })
-      .then((town) => this.tryMoving(town, time, payload))
-      .then((town) => town.notify({ type: 'movement' }));
+      const time = Date.now();
+      const town = await this.tryBuilding(payload.town, time, payload.building);
+      this.emitToTownRoom(payload.town, town, 'town:buildSuccess');
+    } catch (err) {
+      socket.handleError(err, 'build', 'town:buildFail', payload);
+    }
   }
 
-  private static tryBuilding(town: Town, time: number, building: string): PromiseLike<Town> {
-    const target = town.buildings[building];
-    if (target) {
-      const level = target.queued || target.level;
+  private static async recruit(socket: UserSocket, payload: RecruitPayload) {
+    if (!payload.units || !payload.town) { throw new ErrorMessage('Missing required data'); }
+    if (!socket.userData.townIds.includes(payload.town)) { throw new ErrorMessage('No town found'); }
+
+    try {
+      const time = Date.now();
+      const town = await this.tryRecruiting(payload.town, time, payload.units);
+      this.emitToTownRoom(payload.town, town, 'town:recruitSuccess');
+    } catch (err) {
+      socket.handleError(err, 'recruit', 'town:recruitFail', payload);
+    }
+  }
+
+  private static async moveTroops(socket: UserSocket, payload: TroopMovementPayload) {
+    try {
+      if (!payload.town || !payload.target || isNaN(payload.type)) { throw new ErrorMessage('Missing required data'); }
+      if (!socket.userData.townIds.includes(payload.town)) { throw new ErrorMessage('No town found'); }
+
+      const time = Date.now();
+      const town = await this.tryMoving(payload.town, time, payload);
+      this.emitToTownRoom(payload.town, town, 'town:moveTroopsSuccess');
+    } catch (err) {
+      socket.handleError(err, 'movement', 'town:moveTroopsFail', payload);
+    }
+
+    // const time = Date.now();
+    // return Town.getTown({ id: payload.town })
+    //   .then((town) => this.tryMoving(town, time, payload))
+    //   .then((town) => town.notify({ type: 'movement' }));
+  }
+
+  private static async tryBuilding(id: number, time: number, building: string) {
+    const trx = await transaction.start(knexDb.world);
+    try {
       const targetBuilding = worldData.buildingMap[building];
+      if (!targetBuilding) { throw new ErrorMessage('Invalid target building'); }
+
+      const town = await Town.query(trx).findById(id).eager(Town.townRelations);
+      const target = town.buildings[building];
+
+      const level = target.queued || target.level;
+      if (level === targetBuilding.levels.max) { throw new ErrorMessage('Building already at max level'); }
       const buildingData = targetBuilding.data[level];
+      town.resources = town.getResources(time);
 
-      if (!buildingData) {
-        return Promise.reject('Wrong building');
-      }
-      if (!town.checkBuildingRequirements(targetBuilding.requirements)) {
-        return Promise.reject('Requirements not met');
-      }
+      if (!town.doesMeetRequirements(targetBuilding.requirements, 'buildings')) { throw new ErrorMessage('Building requirements not met'); }
+      if (!town.hasEnoughResources(buildingData.costs)) { throw new ErrorMessage('Not enough resources to build'); }
 
-      town = town.updateRes(time);
       town.resources.clay -= buildingData.costs.clay;
       town.resources.wood -= buildingData.costs.wood;
       town.resources.iron -= buildingData.costs.iron;
       target.queued = level + 1;
-      // trigger buildings change manully, because sequelize can't detect it
-      town.changed('buildings', true);
+      const lastQueue = town.getLastQueue('buildingQueues');
+      const endsAt = (lastQueue ? +lastQueue.endsAt : time) + buildingData.buildTime;
 
-      return world.sequelize.transaction((transaction) =>
-        town.createBuildingQueue({
-          building,
-          buildTime: buildingData.buildTime,
-          endsAt: time + buildingData.buildTime,
-          level,
-        }, { transaction })
-          .then(() => town.save({ transaction })),
-      );
-    }
-    // TODO: real error here
-    return Promise.reject('target not found');
-  }
-
-  private static tryRecruiting(town: Town, time: number, units: PayloadUnit[]): PromiseLike<Town> {
-    const unitData = worldData.unitMap;
-    const unitsToQueue = [];
-    const queueCreateTime = new Date();
-    const TownId = town.id;
-    const availablePopulation = town.getAvailablePopulation();
-    const recruitmentModifier = town.getRecruitmentModifier();
-    let usedPop = 0;
-
-    town = town.updateRes(time);
-    for (const unit of units) {
-      const targetUnit = unitData[unit.type];
-      if (!town.units.hasOwnProperty(unit.type) || +unit.amount <= 0) {
-        return Promise.reject('no such unit');
-      }
-      if (!town.checkBuildingRequirements(targetUnit.requirements)) {
-        return Promise.reject('requirements not met');
-      }
-      usedPop += unit.amount;
-
-      town.resources.wood -= targetUnit.costs.wood * unit.amount;
-      town.resources.clay -= targetUnit.costs.clay * unit.amount;
-      town.resources.iron -= targetUnit.costs.iron * unit.amount;
-      town.units[unit.type].queued += unit.amount;
-
-      const lastQueue = town.getLastQueue('UnitQueues');
-      const startTime = lastQueue ? lastQueue.endsAt : queueCreateTime;
-      const recruitTime = unit.amount * targetUnit.recruitTime * recruitmentModifier;
-      const endsAt = startTime.getTime() + recruitTime;
-      unitsToQueue.push({
-        unit: unit.type,
-        amount: unit.amount,
-        recruitTime,
+      await town.$relatedQuery<BuildingQueue>('buildingQueues', trx).insert({
+        level,
         endsAt,
-        TownId,
+        name: building,
+        buildTime: buildingData.buildTime,
       });
-    }
+      await town.$query(trx)
+        .patch({
+          resources: town.resources,
+          buildings: town.buildings,
+          updatedAt: time,
+        })
+        .eager(Town.townRelations)
+        .context({ resourcesUpdated: true });
 
-    if (usedPop > availablePopulation) {
-      return Promise.reject('Population limit exceeded');
-    }
+      await trx.commit();
 
-    town.changed('units', true);
-    return world.sequelize.transaction((transaction) =>
-      UnitQueue.bulkCreate(unitsToQueue, { transaction })
-        .then(() => town.save({ transaction })),
-    );
+      return town;
+    } catch (err) {
+      await trx.rollback();
+      throw err;
+    }
   }
 
-  private static tryMoving(town: Town, time: number, payload: TroopMovementPayload): PromiseLike<Town> {
-    const unitData = worldData.unitMap;
-    let slowest = 0;
+  private static async tryRecruiting(id: number, time: number, units: PayloadUnit[]) {
+    const trx = await transaction.start(knexDb.world);
+    try {
+      const town = await Town.query(trx).findById(id).eager(Town.townRelations);
+      town.resources = town.getResources(time);
+      const unitData = worldData.unitMap;
+      const unitsToQueue = [];
+      const availablePopulation = town.getAvailablePopulation();
+      const recruitmentModifier = town.getRecruitmentModifier();
+      let usedPop = 0;
 
-    if (payload.target === town.location) { return Promise.reject('A town can\'t attack itself'); }
+      for (const unit of units) {
+        const targetUnit = unitData[unit.type];
+        if (!town.units.hasOwnProperty(unit.type) || +unit.amount <= 0) { throw new ErrorMessage('Wrong unit'); }
+        if (!town.doesMeetRequirements(targetUnit.requirements, 'units')) { throw new ErrorMessage('Requirements not met'); }
+        usedPop += unit.amount;
 
-    return Town.findOne({ where: { location: payload.target } }).then((targetTown) => {
+        town.resources.wood -= targetUnit.costs.wood * unit.amount;
+        town.resources.clay -= targetUnit.costs.clay * unit.amount;
+        town.resources.iron -= targetUnit.costs.iron * unit.amount;
+        town.units[unit.type].queued += unit.amount;
+
+        const lastQueue = town.getLastQueue('unitQueues');
+        const startTime = lastQueue ? +lastQueue.endsAt : time;
+        const recruitTime = unit.amount * targetUnit.recruitTime * recruitmentModifier;
+        const endsAt = startTime + recruitTime;
+        unitsToQueue.push({
+          name: unit.type,
+          amount: unit.amount,
+          townId: id,
+          recruitTime,
+          endsAt,
+        });
+      }
+
+      if (usedPop > availablePopulation) { throw new ErrorMessage('Population limit exceeded'); }
+
+      await town.$relatedQuery<UnitQueue>('unitQueues', trx).insert(unitsToQueue);
+      await town.$query(trx)
+        .patch({
+          resources: town.resources,
+          units: town.units,
+          updatedAt: time,
+        })
+        .eager(Town.townRelations)
+        .context({ resourcesUpdated: true });
+
+      await trx.commit();
+
+      return town;
+      } catch (error) {
+        await trx.rollback();
+        throw error;
+      }
+  }
+
+  private static async tryMoving(id: number, time: number, payload: TroopMovementPayload) {
+    const trx = await transaction.start(knexDb.world);
+    try {
+      const unitData = worldData.unitMap;
+      let slowest = 0;
+
+      const town = await Town.query(trx).findById(id).eager(Town.townRelations);
+      if (payload.target === town.location) { throw new ErrorMessage('A town can\'t attack itself'); }
+
+      const targetTown = await Town.query(trx).findOne({ location: payload.target }).eager(Town.townRelations);
+      if (!targetTown) { throw new ErrorMessage('Invalid target'); }
+
       const distance = Town.calculateDistance(town.location, targetTown.location);
-
+      const units = {};
       for (const unit of payload.units) {
-        if (!town.units.hasOwnProperty(unit[0])) { return Promise.reject('No such unit'); }
+        if (!town.units.hasOwnProperty(unit[0])) { throw new ErrorMessage('No such unit'); }
 
+        units[unit[0]] = unit[1];
         town.units[unit[0]].inside -= unit[1];
         town.units[unit[0]].outside += unit[1];
         slowest = Math.max(unitData[unit[0]].speed, slowest);
       }
-
       const movementTime = slowest * distance;
-      town.changed('units', true);
-      return world.sequelize.transaction((transaction) =>
-        town.createMovementOriginTown({
-          units: payload.units.reduce((result, [name, count]) => ({ ...result, [name]: count }), {}),
-          type: payload.type,
-          endsAt: time + movementTime,
-          MovementDestinationId: targetTown.id,
-        }, { transaction })
-          .then(() => town.save({ transaction })),
-      );
-    });
-  }
-  private static update(socket: UserSocket, payload: SocketPayload) {
-    if (!payload.town) { return Promise.reject('No town specified'); }
-
-    const time = new Date();
-    return Town.processTownQueues(payload.town, time)
-      .then(({ town, processed }) => {
-        const processedAttack = processed.some((queue) =>
-          queue.constructor.name === 'Movement' && queue.type === 'attack');
-
-        // TODO: handle town rooms on lose as well
-        if (!processedAttack) { return town.notify({ type: 'update' }); }
-
-        // Refresh whole player in case of a lost town
-        return Player.getPlayer({ UserId: socket.userData.userId })
-          .then((player) => {
-            socket.userData = {
-              ...socket.userData,
-              townIds: player.Towns.map(({ id }) => id),
-            };
-            socket.emit('player', player);
-          });
+      await town.$relatedQuery<Movement>('originMovements', trx).insert({
+        units,
+        type: payload.type,
+        targetTownId: targetTown.id,
+        endsAt: time + movementTime,
       });
+      await town.$query(trx)
+        .patch({
+          units: town.units,
+        })
+        .eager(Town.townRelations);
+      await trx.commit();
+
+      return town;
+    } catch (err) {
+      await trx.rollback();
+      throw err;
+    }
+  }
+
+  private static update(socket: UserSocket, payload: SocketPayload) {
+    // if (!payload.town) { return Promise.reject('No town specified'); }
+
+    // const time = new Date();
+    // return Town.processTownQueues(payload.town, time)
+    //   .then(({ town, processed }) => {
+    //     const processedAttack = processed.some((queue) =>
+    //       queue.constructor.name === 'Movement' && queue.type === 'attack');
+
+    //     // TODO: handle town rooms on lose as well
+    //     if (!processedAttack) { return town.notify({ type: 'update' }); }
+
+    //     // Refresh whole player in case of a lost town
+    //     return Player.getPlayer({ UserId: socket.userData.userId })
+    //       .then((player) => {
+    //         socket.userData = {
+    //           ...socket.userData,
+    //           townIds: player.Towns.map(({ id }) => id),
+    //         };
+    //         socket.emit('player', player);
+    //       });
+    //   });
   }
 }
