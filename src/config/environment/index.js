83833fe91833e4578b40fc8897f0c8514f6508bf
Klemensas <hello@klemensas.net>
2018-04-08 00:18:44 +0300

    Actually use environment files

--- src/config/environment/index.js
+++ src/config/environment/index.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
+const env = process.env.NODE_ENV || 'development';
+const envVariables = require(`./${env}`);
+
 module.exports = {
-  env: process.env.NODE_ENV || 'development',
+  env,
   ip: process.env.IP || '0.0.0.0',
   port: process.env.PORT || 9000,
   root: path.normalize(path.join(__dirname, '/../../..')),
@@ -18,6 +21,7 @@ module.exports = {
       },
     },
     main: process.env.DB_MAIN || 'postgres://stratego:supasecretpassword@localhost:5432/stratego',
-    world: process.env.DB_MAIN || 'postgres://stratego:supasecretpassword@localhost:5432/strategoWorld'
-  }
+    world: process.env.DB_WORLD || 'postgres://stratego:supasecretpassword@localhost:5432/strategoWorld'
+  },
+  ...envVariables,
 };
