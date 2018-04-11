2bb063313ace97c56558851b8eee080ac371cfb2
Klemensas Dranseika <hello@klemensas.net>
2018-04-11 10:49:00 +0300

    Set test environmnet and use environment files

--- src/config/environment/index.js
+++ src/config/environment/index.js
@@ -1,8 +1,16 @@
 const path = require('path');
+const test = require('./test');
+const development = require('./development');
+const production = require('./production');
 
+// TODO: cleanup here, take a fresh look at variables and import only the required env
 const env = process.env.NODE_ENV || 'development';
-const envVariables = require(`./${env}`);
 
+const envVariables = {
+  test,
+  development,
+  production,
+};
 module.exports = {
   env,
   ip: process.env.IP || '0.0.0.0',
@@ -23,5 +31,5 @@ module.exports = {
     main: process.env.DB_MAIN || 'postgres://stratego:supasecretpassword@localhost:5432/stratego',
     world: process.env.DB_WORLD || 'postgres://stratego:supasecretpassword@localhost:5432/strategoWorld'
   },
-  ...envVariables,
+  ...envVariables[env],
 };
