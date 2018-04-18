c22fa9b1f017ed0eb5a1ba7f23458546142adda1
Klemensas <hello@klemensas.net>
2018-04-18 16:09:17 +0300

    Move more variables to environment for seeding

--- seeds/index.ts
+++ seeds/index.ts
@@ -12,19 +12,33 @@ const baseProduction = config.seed.baseProduction;
 const queueRate = config.seed.queueRate;
 const queueCount = config.seed.queueCount;
 const queueSpread = config.seed.queueSpread;
+const townPercent = config.seed.townPercent;
+const townArea = config.seed.townArea;
+const townDistance = config.seed.townDistance;
 const maxDemoTowns = 5;
 const demoTownRate = 0.4;
 const seedString = 'megapolis';
 
 (async () => {
-  console.log('Seeding in progress, this might take a while, please wait');
+  console.log('Seeding in progress, this might take a while, please wait', demoUserCount, queueCount);
   console.time('seed');
   try {
     const mainData = await seedMain(knexDb.main, speed, demoUserCount);
     const users = mainData[0];
     const world = mainData[1][0];
 
-    const worldData = await seedWorld(knexDb.world, users, world, maxDemoTowns, demoTownRate, speed, baseProduction);
+    const worldData = await seedWorld(
+      knexDb.world,
+      users,
+      world,
+      maxDemoTowns,
+      demoTownRate,
+      speed,
+      baseProduction,
+      townPercent,
+      townArea,
+      townDistance,
+    );
     await seedQueues(knexDb.world, seedString, worldData.towns, queueRate, queueCount, queueSpread);
 
     console.timeEnd('seed');
