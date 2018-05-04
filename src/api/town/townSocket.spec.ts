739c41bfa3bc6ceab6b2d3498c40c647a1d1a083
Klemensas <hello@klemensas.net>
2018-05-04 14:45:34 +0300

    Add townSocket tests

--- src/api/town/townSocket.spec.ts
+++ src/api/town/townSocket.spec.ts
@@ -1,4 +1,5 @@
 import { transaction } from 'objection';
+import { EventEmitter } from 'events';
 
 import { TownSocket } from './townSocket';
 import { TownSupport } from './townSupport';
@@ -9,8 +10,15 @@ import { UserSocket, ErrorMessage } from '../../config/socket';
 import { townQueue } from '../townQueue';
 import { Movement } from './movement';
 import * as townQueries from './townQueries';
+import { NamePayload, BuildPayload, RecruitPayload, TroopMovementPayload, MovementType } from 'strat-ego-common';
 
-beforeAll(() => {
+let socket: UserSocket;
+let emitSpy;
+beforeEach(() => {
+  socket = new EventEmitter() as UserSocket;
+  socket.handleError = jest.fn().mockImplementationOnce(() => null);
+  emitSpy = jest.spyOn(TownSocket, 'emitToTownRoom').mockImplementationOnce(() => null);
+  emitSpy.mockReset();
   worldData.unitMap = {
     sword: { speed: 1 },
     archer: { speed: 2 },
@@ -19,6 +27,92 @@ beforeAll(() => {
   worldData.world = {
     baseProduction: 500,
   } as World;
+});
+
+describe('onConnect', () => {
+  const socketEvents = [
+    'town:rename',
+    'town:build',
+    'town:recruit',
+    'town:moveTroops',
+    'town:recallSupport',
+    'town:sendBackSupport',
+  ];
+
+  test('should register events', () => {
+    jest.spyOn(TownSocket, 'joinTownRoom').mockImplementationOnce(() => null);
+    expect(socket.eventNames()).toHaveLength(0);
+    TownSocket.onConnect(socket);
+    expect(TownSocket.joinTownRoom).toHaveBeenCalledWith(socket);
+    expect(socket.eventNames()).toEqual(socketEvents);
+  });
+
+  describe('events', () => {
+    beforeEach(() => {
+      jest.spyOn(TownSocket, 'joinTownRoom').mockImplementationOnce(() => null);
+      TownSocket.onConnect(socket);
+    });
+
+    test('should call rename on rename emit', () => {
+      jest.spyOn(TownSocket, 'rename').mockImplementationOnce(() => null);
+      socket.emit('anything');
+      expect(TownSocket.rename).not.toHaveBeenCalled();
+
+      const payload: NamePayload = { town: 1, name: 'test' };
+      socket.emit('town:rename', payload);
+      expect(TownSocket.rename).toHaveBeenCalledWith(socket, payload);
+    });
+
+    test('should call build on build emit', () => {
+      jest.spyOn(TownSocket, 'build').mockImplementationOnce(() => null);
+      socket.emit('anything');
+      expect(TownSocket.build).not.toHaveBeenCalled();
+
+      const payload: BuildPayload = { town: 1, building: 'test' };
+      socket.emit('town:build', payload);
+      expect(TownSocket.build).toHaveBeenCalledWith(socket, payload);
+    });
+
+    test('should call recruit on recruit emit', () => {
+      jest.spyOn(TownSocket, 'recruit').mockImplementationOnce(() => null);
+      socket.emit('anything');
+      expect(TownSocket.recruit).not.toHaveBeenCalled();
+
+      const payload: RecruitPayload = { town: 1, units: [{ type: 'test', amount: 1 }] };
+      socket.emit('town:recruit', payload);
+      expect(TownSocket.recruit).toHaveBeenCalledWith(socket, payload);
+    });
+
+    test('should call moveTroops on moveTroops emit', () => {
+      jest.spyOn(TownSocket, 'moveTroops').mockImplementationOnce(() => null);
+      socket.emit('anything');
+      expect(TownSocket.moveTroops).not.toHaveBeenCalled();
+
+      const payload: TroopMovementPayload = { town: 1, target: [1, 1], units: { test: 1 }, type: MovementType.attack };
+      socket.emit('town:moveTroops', payload);
+      expect(TownSocket.moveTroops).toHaveBeenCalledWith(socket, payload);
+    });
+
+    test('should call cancelSupport on recallSupport emit', () => {
+      jest.spyOn(TownSocket, 'cancelSupport').mockImplementationOnce(() => null);
+      socket.emit('anything');
+      expect(TownSocket.cancelSupport).not.toHaveBeenCalled();
+
+      const payload = 2;
+      socket.emit('town:recallSupport', payload);
+      expect(TownSocket.cancelSupport).toHaveBeenCalledWith(socket, payload, 'origin');
+    });
+
+    test('should call cancelSupport on sendBackSupport emit', () => {
+      jest.spyOn(TownSocket, 'cancelSupport').mockImplementationOnce(() => null);
+      socket.emit('anything');
+      expect(TownSocket.cancelSupport).not.toHaveBeenCalled();
+
+      const payload = 2;
+      socket.emit('town:sendBackSupport', payload);
+      expect(TownSocket.cancelSupport).toHaveBeenCalledWith(socket, payload, 'target');
+    });
+  });
 });
 
 describe('cancelSupport', () => {
@@ -43,19 +137,14 @@ describe('cancelSupport', () => {
       location: [2, 2],
     },
   };
-  let socket: UserSocket;
-  const getSupportSpy = jest.spyOn(townQueries, 'getTownSupport');
-  const deleteSupportSpy = jest.spyOn(townQueries, 'deleteSupport');
-  let emitSpy;
+  let getSupportSpy;
+  let deleteSupportSpy;
   beforeEach(() => {
-    socket = {
-      handleError: jest.fn().mockImplementationOnce(() => null),
-      userData: {
-        townIds: [support.originTown.id, support.targetTown.id],
-      },
-    } as any;
-    emitSpy = jest.spyOn(TownSocket, 'emitToTownRoom').mockImplementationOnce(() => null);
-    emitSpy.mockReset();
+    getSupportSpy = jest.spyOn(townQueries, 'getTownSupport');
+    deleteSupportSpy = jest.spyOn(townQueries, 'deleteSupport');
+    socket.userData = {
+      townIds: [support.originTown.id, support.targetTown.id],
+    };
   });
 
   test('should rollback transaction and call socket handler', async () => {
