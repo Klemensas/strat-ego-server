4be28e26509a78d88b49a7f8ad652da97398d7f5
Klemensas <hello@klemensas.net>
2018-06-24 13:35:35 +0300

    Set higher growth interval

--- migrations/main/20180528121550_town_growth.js
+++ migrations/main/20180528121550_town_growth.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.table('World', table => {
-    table.integer('townGrowthInterval').unsigned().notNullable().defaultTo(1000000);
+    table.integer('townGrowthInterval').unsigned().notNullable().defaultTo(14400000);
     table.bigInteger('townLastGrowth').unsigned().notNullable().defaultTo(Date.now());
   });
 };
